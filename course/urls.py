1d5ec6cbba8f15075b5e0ca6c4bc9137885aac2c
Mamunur1093 <mamunur1093@gmail.com>
2025-06-21 12:22:15 +0600

    Course Create And show list

--- course/urls.py
+++ course/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('course/', views.course_index, name='course_index'),
+    path('course/create/', views.course_create, name='course_create'),
+    
 ]
--- course/views.py
+++ course/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Course
 # Create your views here.
 def course_index(request):
-    return render(request, 'course/index.html')
+    courses = Course.objects.all()
+    return render(request, 'course/index.html', {'courses': courses})
+
+def course_create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        course_code = request.POST.get('course_code')
+        credit = request.POST.get('credit')
+        price = request.POST.get('price')
+
+        course = Course(name=name, course_code=course_code, credit=credit, price=price)
+        course.save()
+        return redirect('course_index')
+
+    return render(request, 'course/create.html')
