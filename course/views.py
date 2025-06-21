6b2d031a23c3ca5c25d40c925ac2456035a87b41
Mamunur1093 <mamunur1093@gmail.com>
2025-06-21 14:28:29 +0600

    course edit error fix

--- course/views.py
+++ course/views.py
@@ -23,12 +23,11 @@ def course_create(request):
 def course_edit(request, course_id):
     course = Course.objects.get(id=course_id)
     if request.method == 'POST':
-        name = request.POST.get('name')
-        course_code = request.POST.get('course_code')
-        credit = request.POST.get('credit')
-        price = request.POST.get('price')
-
-        course = Course(name=name, course_code=course_code, credit=credit, price=price)
+        course.name = request.POST.get('name')
+        course.course_code = request.POST.get('course_code')
+        course.credit = request.POST.get('credit')
+        course.price = request.POST.get('price')
+        
         course.save()
         messages.add_message(request, messages.SUCCESS, "Course updated successfully")
         return redirect('course_index')
